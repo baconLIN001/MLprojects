e011daa30d4d512ece1e3aece7d9b4806a76d140
baconLIN <baconLIN@outlook.com>
2017-03-28 12:41:07 +0800

    kNN and decision tree

--- CART/test.py
+++ CART/test.py
@@ -7,6 +7,6 @@ from numpy import *
 testMat = mat(eye(4))
 print testMat
 
-mat0,mat1 = regTrees.binSplitDataSet(testMat,1,0.5)
+mat0, mat1 = regTrees.binSplitDataSet(testMat,1,0.5)
 print mat0
 print mat1
